595d583f7f9be9aa700187b398231d5466f3eb89
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-09-16 10:15:14 +0500

    написал код

--- Hometask 2/Program.cs
+++ Hometask 2/Program.cs
@@ -6,3 +6,18 @@
 
 */
 
+Console.WriteLine("Введите первое число: ");
+int numOne = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numTwo = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numThree = int.Parse(Console.ReadLine());
+int max = numOne;
+if (numOne<numTwo)
+{
+    max = numTwo;
+    if (numTwo<numThree)
+    max = numThree;
+} else if (numOne<numThree)
+max = numThree;
+Console.WriteLine($"Наибольшее из трех чисел: {max}");

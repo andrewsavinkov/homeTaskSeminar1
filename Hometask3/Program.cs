78d473fb785358b240cdfff3607b2db404425e38
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-09-18 16:04:43 +0500

    написал код

--- Hometask3/Program.cs
+++ Hometask3/Program.cs
@@ -6,3 +6,9 @@
 
 */
 
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+Console.WriteLine($"Число {num} четное!");
+else
+Console.WriteLine($"Число {num} нечетное!");

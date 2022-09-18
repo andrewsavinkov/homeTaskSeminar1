58d2f630f2a279fee957cb016d9d1a34875d584b
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-09-18 16:23:26 +0500

    написал код

--- Hometask4/Program.cs
+++ Hometask4/Program.cs
@@ -2,3 +2,18 @@
 
 5 -> 2, 4
 8 -> 2, 4, 6, 8 */
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int i = 2;
+if (num < 2)
+Console.WriteLine("Введите число побольше!");
+else
+{
+    Console.WriteLine("Ваши числа: ");
+    while (i<=num)
+    {
+        Console.WriteLine($"{i}");
+        i = i+2;
+    }
+}
